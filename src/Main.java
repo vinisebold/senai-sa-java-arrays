f2eafe4c4d7774c5e69d84c12ebe8e4ee1fef03d
vinisebold <vinicius.sebold05@gmail.com>
2025-04-28 11:18:00 -0300

    feat: solicita o número de vendedores e seus nomes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        //Inicializa scanner
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        //Entrada de dados sobre os vendedores
+        System.out.println("Qual o número de vendedores?");
+        int qntdVendedores = sc.nextInt();
+
+        //Declaração do vetor
+        String[] nomesVendedores = new String[qntdVendedores];
+
+        for (int i = 0; i < qntdVendedores; i++) {
+            System.out.println("Qual o nome do vendedor " + (i + 1));
+            nomesVendedores[i] = sc.next();
+        }
+        for (int j = 0; j < nomesVendedores.length; j++) {
+            System.out.println("Nome[" + (j + 1) + "] " + nomesVendedores[j]);
         }
     }
 }
