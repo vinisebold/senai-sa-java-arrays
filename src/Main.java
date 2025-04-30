9b530232c9ff5fea6e000a9fcadb11ec00d3afeb
vinisebold <vinicius.sebold05@gmail.com>
2025-04-30 12:18:21 -0300

    feat: altera a lógica do laço For para entrada de valores dos dias

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Entrada de dados sobre os vendedores
-        System.out.println("Qual o número de vendedores?");
+        System.out.print("\nInforme o número de vendedores: ");
         int qntdVendedores = sc.nextInt();
 
         //Declaração do vetor
         String[] nomesVendedores = new String[qntdVendedores];
-
-        for (int i = 0; i < qntdVendedores; i++) {
-            System.out.println("Qual o nome do vendedor " + (i + 1));
-            nomesVendedores[i] = sc.next();
-        }
-        //for (int j = 0; j < nomesVendedores.length; j++) {
-        //    System.out.println("Nome[" + (j + 1) + "] " + nomesVendedores[j]);
-        //}
-
         Double[][] qntdVendas = new Double[qntdVendedores][5];
 
         for (int i = 0; i < qntdVendedores; i++) {
-
-            System.out.println("Digite o valor de vendas de " + nomesVendedores[i]);
+            System.out.print("\nInforme o nome do vendedor " + (i + 1) + ": ");
+            nomesVendedores[i] = sc.next();
+            System.out.println("\nDigite o valor de vendas de " + nomesVendedores[i]);
             for (int j = 0; j < 5; j++) {
-                System.out.println("Dia " + j);
+                System.out.print("Dia " + (j + 1) + ": ");
                 qntdVendas[i][j] = sc.nextDouble();
             }
         }
