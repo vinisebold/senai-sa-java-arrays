017756986d2f741bc2b3212b6a4acc6b70c840d4
vinisebold <vinicius.sebold05@gmail.com>
2025-05-07 11:42:24 -0300

    feat(Main): Implementa laço para somar vendas em um array

--- src/SituaçãoAprendizagem/Main.java
+++ src/SituaçãoAprendizagem/Main.java
@@ -14,14 +14,18 @@ public class Main {
         //Declaração do vetor
         String[] nomesVendedores = new String[qntdVendedores];
         Double[][] qntdVendas = new Double[qntdVendedores][5];
+        Double[] somaVendas = new Double[qntdVendedores];
 
+        // Laço de repetição
         for (int i = 0; i < qntdVendedores; i++) {
+            somaVendas[i] = 0.0;
             System.out.print("\nInforme o nome do vendedor " + (i + 1) + ": ");
             nomesVendedores[i] = sc.next();
             System.out.println("\nDigite o valor de vendas de " + nomesVendedores[i]);
             for (int j = 0; j < 5; j++) {
                 System.out.print("Dia " + (j + 1) + ": ");
                 qntdVendas[i][j] = sc.nextDouble();
+                somaVendas[i] += qntdVendas[i][j];
             }
         }
     }
