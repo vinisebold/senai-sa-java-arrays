9f2bc3a9caf065ddfa4157b05b3a8615725cf429
vinisebold <vinicius.sebold05@gmail.com>
2025-05-07 12:18:33 -0300

    feat(Main): Implementa laço para exibir o relatório

--- src/SituaçãoAprendizagem/Main.java
+++ src/SituaçãoAprendizagem/Main.java
@@ -16,7 +16,7 @@ public class Main {
         Double[][] qntdVendas = new Double[qntdVendedores][5];
         Double[] somaVendas = new Double[qntdVendedores];
 
-        // Laço de repetição
+        // Laço de repetição para entrada de dados
         for (int i = 0; i < qntdVendedores; i++) {
             somaVendas[i] = 0.0;
             System.out.print("\nInforme o nome do vendedor " + (i + 1) + ": ");
@@ -26,6 +26,17 @@ public class Main {
                 System.out.print("Dia " + (j + 1) + ": ");
                 qntdVendas[i][j] = sc.nextDouble();
                 somaVendas[i] += qntdVendas[i][j];
+            }
+        }
+
+        // Laço de repetição para relatório
+        for (int a = 0; a < qntdVendedores; a++){
+            if (somaVendas[a] >= 5000.0){
+                System.out.println(nomesVendedores[a] + " | " + somaVendas[a] + "| Excelente |" + somaVendas[a] * 1.1);
+            } else if (somaVendas[a] > 3000.0 && somaVendas[a] < 4999.99) {
+                System.out.println(nomesVendedores[a] + " | " + somaVendas[a] + "| Bom |" + somaVendas[a] * 1.05);
+            } else {
+                System.out.println(nomesVendedores[a] + " | " + somaVendas[a] + "| Regular | R$ 0,00");
             }
         }
     }
