37a5415f5cce128f74f74e2b9b68f36a7ecdb0a9
vinicius.sebold05@gmail.com <vinicius.sebold05@gmail.com>
2025-05-12 11:42:11 -0300

    feat(main): define melhor vendedor da semana, implementa formato do relatório final

---
+++ src/SituacaoAprendizagem/Main.java
@@ -0,0 +1,64 @@
+package SituacaoAprendizagem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Inicializa scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Entrada de dados sobre os vendedores
+        System.out.print("\nInforme o número de vendedores: ");
+        int qntdVendedores = sc.nextInt();
+
+        //Declaração do vetor
+        String[] nomesVendedores = new String[qntdVendedores];
+        Double[][] qntdVendas = new Double[qntdVendedores][5];
+        Double[] somaVendas = new Double[qntdVendedores];
+
+        // Laço de repetição para entrada de dados
+        for (int i = 0; i < qntdVendedores; i++) {
+            somaVendas[i] = 0.0;
+            System.out.print("\nInforme o nome do vendedor " + (i + 1) + ": ");
+            nomesVendedores[i] = sc.next();
+            System.out.println("\nDigite o valor de vendas de " + nomesVendedores[i]);
+            for (int j = 0; j < 5; j++) {
+                System.out.print("Dia " + (j + 1) + ": ");
+                qntdVendas[i][j] = sc.nextDouble();
+                somaVendas[i] += qntdVendas[i][j];
+            }
+        }
+        System.out.println("\n============================================================");
+        System.out.println(" RELATÓRIO DE VENDAS SEMANAIS");
+        System.out.println("============================================================");
+        System.out.printf("%-16s | %-13s | %-13s | %-8s%n", "Vendedor", "Total Vendido", "Classificação", "Bônus");
+        System.out.println("+-----------------------------------------------------------------------------+");
+        // Laço de repetição para relatório
+        for (int a = 0; a < qntdVendedores; a++) {
+            if (somaVendas[a] >= 5000.0) {
+                System.out.printf("%-16s | R$ %10.2f | %-13s | R$ %8.2f\n", nomesVendedores[a], somaVendas[a], "Exelente", somaVendas[a] * 1.1);
+            } else if (somaVendas[a] > 3000.0 && somaVendas[a] < 4999.99) {
+                System.out.printf("%-16s | R$ %10.2f | %-13s | R$ %8.2f\n", nomesVendedores[a], somaVendas[a], "Bom", somaVendas[a] * 1.05);
+            } else {
+                System.out.printf("%-16s | %10.2s | %-13s | %8.2s\n", nomesVendedores[a], somaVendas[a], "Regular", "R$ 0,00");
+            }
+        }
+
+        System.out.println("+-----------------------------------------------------------------------------+");
+
+        // Encontrar a vendedora com maior valor de vendas
+        double maiorVenda = somaVendas[0];
+        int indiceMelhorVendedora = 0;
+
+        for (int j = 1; j < qntdVendedores; j++) {
+            if (somaVendas[j] > maiorVenda) {
+                maiorVenda = somaVendas[j];
+                indiceMelhorVendedora = j;
+            }
+        }
+
+        System.out.println("\nMELHOR VENDEDORA DA SEMANA: " + nomesVendedores[indiceMelhorVendedora] + " (R$ " + maiorVenda + ")");
+
+        System.out.println("============================================================");
+    }
+}
